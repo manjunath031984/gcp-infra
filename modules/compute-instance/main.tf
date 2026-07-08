293157a80cc72c7cd0813c860ae357fec20740c3
manjunath031984 <85352670+manjunath031984@users.noreply.github.com>
2026-07-09 00:25:26 +0530

    Add files via upload

---
+++ modules/compute-instance/main.tf
@@ -0,0 +1,39 @@
+resource "google_compute_instance" "vm" {
+
+  name         = var.vm_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+
+    initialize_params {
+
+      image = "projects/debian-cloud/global/images/family/debian-12"
+
+      size = 20
+
+      type = "pd-balanced"
+    }
+  }
+
+  network_interface {
+
+    network = "default"
+
+    access_config {}
+  }
+
+  metadata = {
+    enable-oslogin = "TRUE"
+  }
+
+  labels = {
+    environment = "dev"
+    owner       = "terraform"
+  }
+
+  tags = [
+    "terraform",
+    "gcp"
+  ]
+}
---
+++ modules/compute-instance/variables.tf
@@ -0,0 +1,11 @@
+variable "vm_name" {
+  type = string
+}
+
+variable "machine_type" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
